d459ef4b19e28f5c52d39e5504eaa438e54c12bd
limin <lim6@haihangyun.com>
2018-09-14 13:43:28 +0800

    update the push_autorizedkeys

--- roles/installmysql/files/user_clear.sh
+++ roles/installmysql/files/user_clear.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 mysql -uroot -padminpwd -e "use mysql;delete from user where password=\"\";flush privileges;"
 mysql -uroot -padminpwd -e "grant all privileges on *.* to root@'%' identified by 'adminpwd';flush privileges;"
-
